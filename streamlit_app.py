569f02ad3326a1521d38de73420326d823340cb2
RomanHaak <66518859+RomanHaak@users.noreply.github.com>
2023-12-17 15:07:10 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,18 +6,14 @@ import requests
 from io import BytesIO
 
 # URL der Excel-Datei auf GitHub
-url = 'https://github.com/RomanHaak/streamlit-example/blob/e1ac932997152b8c602b3aa54fd404a3b5f35520/CBAM_Estimator_Default_Values.xlsx'
+url = 'https://raw.githubusercontent.com/RomanHaak/streamlit-example/master/CBAM_Estimator_Default_Values.xlsx'
+
 
 # Anfordern der Datei von GitHub
 response = requests.get(url)
 response.raise_for_status()  # Stellt sicher, dass der Request erfolgreich war
 
-# Lesen der Excel-Datei aus dem Response
-# Lesen des spezifischen Bereichs A6 bis DR282 vom Blatt 'Alle_Default_Values_Mit_Nullen'
-data_frame = pd.read_excel(BytesIO(response.content), sheet_name='Alle_Default_Values_Mit_Nullen', skiprows=5, usecols='A:DR', nrows=277)
 
-# Umwandeln des DataFrame in eine Matrix (Liste von Listen)
-matrix_data = data_frame.values.tolist()
 
 
 
